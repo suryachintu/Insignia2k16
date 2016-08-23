1b874d389720c1dcb56f34cca05832d48af5c1e4
suryachintu <suryaparsa@gmail.com>
2016-08-24 01:38:39 +0530

    Add surya's activities

---
+++ app/src/main/java/com/example/surya/insignia2k16/events_main/MainActivity.java
@@ -0,0 +1,262 @@
+package com.example.surya.insignia2k16.events_main;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.surya.insignia2k16.R;
+import com.example.surya.insignia2k16.chat.GlobalChat;
+import com.example.surya.insignia2k16.chat.auth.Login;
+import com.example.surya.insignia2k16.instafeed.Instafeed;
+import com.example.surya.insignia2k16.locate.MapsActivity;
+import com.google.android.gms.appinvite.AppInvite;
+import com.google.android.gms.appinvite.AppInviteInvitation;
+import com.google.android.gms.appinvite.AppInviteInvitationResult;
+import com.google.android.gms.appinvite.AppInviteReferral;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener ,GoogleApiClient.OnConnectionFailedListener {
+
+
+    public static final int REQUEST_INVITE=0;
+    FirebaseAuth mFirebaseAuth;
+    DatabaseReference mReference;
+    FirebaseUser mFirebaseUser;
+    RecyclerView mRecyclerView;
+    GoogleApiClient mGoogleApiClient;
+    ImageView mImageView;
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*newly added* */
+
+        //vengal made photo display in navbar section
+        SharedPreferences sharedPreferences=getSharedPreferences("UserPrefs",MODE_PRIVATE);
+        String s=sharedPreferences.getString("Profile",null);
+        String login=sharedPreferences.getString("USERNAME",null);
+
+//        mImageView=(ImageView)findViewById(R.id.profile_imag_main);
+//
+//        if(s!=null&&s!="a")
+//        Picasso.with(MainActivity.this).load(Uri.parse(s)).fit().into(mImageView);
+//        mTextView=(TextView)findViewById(R.id.user_login_detail) ;
+        //mTextView.setText(login);
+        // Create an auto-managed GoogleApiClient with access to App Invites.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(AppInvite.API)
+                .enableAutoManage(this, this)
+                .build();
+
+        // Check for App Invite invitations and launch deep-link activity if possible.
+        // Requires that an Activity is registered in AndroidManifest.xml to handle
+        // deep-link URLs.
+        boolean autoLaunchDeepLink = true;
+        AppInvite.AppInviteApi.getInvitation(mGoogleApiClient, this, autoLaunchDeepLink)
+                .setResultCallback(
+                        new ResultCallback<AppInviteInvitationResult>() {
+                            @Override
+                            public void onResult(AppInviteInvitationResult result) {
+                                Log.d("vengal", "getInvitation:onResult:" + result.getStatus());
+                                if (result.getStatus().isSuccess()) {
+                                    // Extract information from the intent
+                                    Intent intent = result.getInvitationIntent();
+                                    String deepLink = AppInviteReferral.getDeepLink(intent);
+                                    String invitationId = AppInviteReferral.getInvitationId(intent);
+
+                                    // Because autoLaunchDeepLink = true we don't have to do anything
+                                    // here, but we could set that to false and manually choose
+                                    // an Activity to launch to handle the deep link here.
+                                    // ...
+                                }
+                            }
+                        });
+
+
+        //recycler view
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        Events_Adapter myAdapter = new Events_Adapter();
+
+        mRecyclerView.setAdapter(myAdapter);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mReference = FirebaseDatabase.getInstance().getReference();
+
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+//       myAdapter.SetOnItemClickListener(new Events_Adapter().OnItemClickListener() {
+//            @Override
+//            public void onItemClick(View view, int position) {
+//                Log.e("xxxxxx","cxxxxxxxxxxxxxx");
+//                Toast.makeText(MainActivity.this, Constants.mEvents_names[position] + position, Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(MainActivity.this,detail_events.class);
+//                intent.putExtra("p",position);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        myAdapter.SetOnItemClickListener(new Events_Adapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(View view, int position) {
+//                Log.e("xxxxxx","cxxxxxxxxxxxxxx");
+//                Toast.makeText(MainActivity.this, Constants.mEvents_names[position] + position, Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(MainActivity.this,detail_events.class);
+//                intent.putExtra("p",position);
+//                startActivity(intent);
+//            }
+//        });
+
+        /*end of newly added*/
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_chat) {
+
+            startActivity(new Intent(MainActivity.this,GlobalChat.class));
+
+        } else if (id == R.id.nav_Instafeed) {
+
+            startActivity(new Intent(MainActivity.this, Instafeed.class));
+
+        } else if (id == R.id.nav_about) {
+
+//            startActivity(new Intent(MainActivity.this, AboutInsignia.class));
+
+        } else if (id == R.id.nav_locate) {
+
+            startActivity(new Intent(MainActivity.this, MapsActivity.class));
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("vengal", "onActivityResult: requestCode=" + requestCode + ", resultCode=" + resultCode);
+
+        if (requestCode == REQUEST_INVITE) {
+            if (resultCode == RESULT_OK) {
+                // Get the invitation IDs of all sent messages
+                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
+                for (String id : ids) {
+                    Log.d("vengal", "onActivityResult: sent invitation " + id);
+                }
+            } else {
+                // Sending failed or it was canceled, show failure message to the user
+                // ...
+            }
+        }
+    }
+
+    private void onInviteClicked() {
+        Intent intent = new AppInviteInvitation.IntentBuilder(getString(R.string.invitation_title))
+                .setMessage(getString(R.string.invitation_message))
+                .setCallToActionText(getString(R.string.invitation_cta))
+                .build();
+        startActivityForResult(intent,REQUEST_INVITE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mFirebaseUser == null){
+            Toast.makeText(MainActivity.this, "null", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this,Login.class));
+            finish();
+            return;
+        }
+    }
+
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
